6bdbbf4c1308f605efb2c39e85b2f179a687b5b2
Jaredt Hultink <JHultink@rpmmoves.com>
2024-10-16 12:41:06 -0400

    Added calling APIs

--- flutter_firebase_test/lib/main.dart
+++ flutter_firebase_test/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,6 +41,20 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String? phoneNumber;
   bool showConfirmation = false;
+  User? user;
+  String? getDriverResponse;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      log('Auth state changed. User: ${user?.toString()}');
+      setState(() {
+        this.user = user;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,72 +64,103 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Firebase Test'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Enter phone number beginning with country code (+1)'),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter phone number',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    phoneNumber = value;
-                  });
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                if (phoneNumber != null) {
-                  final auth = FirebaseAuth.instance;
-
-                  await auth.verifyPhoneNumber(
-                    phoneNumber: phoneNumber!,
-                    verificationCompleted:
-                        (PhoneAuthCredential credential) async {
-                      log('Verification completed: $credential');
-                      await auth.signInWithCredential(credential);
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                if (user == null) ...[
+                  const Text(
+                      'Enter phone number beginning with country code (+1)'),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter phone number',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        phoneNumber = value;
+                      });
                     },
-                    verificationFailed: (FirebaseAuthException e) {
-                      log('Verification failed: $e');
-                    },
-                    codeSent: (String verificationId, int? resendToken) async {
-                      log('Code sent: $verificationId');
-                      String? smsCode = await showConfirmationDialog();
-                      if (smsCode != null) {
-                        // Create a PhoneAuthCredential with the code
-                        PhoneAuthCredential credential =
-                            PhoneAuthProvider.credential(
-                                verificationId: verificationId,
-                                smsCode: smsCode);
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (phoneNumber != null) {
+                        final auth = FirebaseAuth.instance;
 
-                        // Sign the user in (or link) with the credential
-                        final user =
+                        await auth.verifyPhoneNumber(
+                          phoneNumber: phoneNumber!,
+                          verificationCompleted:
+                              (PhoneAuthCredential credential) async {
+                            log('Verification completed: $credential');
                             await auth.signInWithCredential(credential);
-                        log('User: $user');
+                          },
+                          verificationFailed: (FirebaseAuthException e) {
+                            log('Verification failed: $e');
+                          },
+                          codeSent:
+                              (String verificationId, int? resendToken) async {
+                            log('Code sent: $verificationId');
+                            String? smsCode = await showConfirmationDialog();
+                            if (smsCode != null) {
+                              // Create a PhoneAuthCredential with the code
+                              PhoneAuthCredential credential =
+                                  PhoneAuthProvider.credential(
+                                      verificationId: verificationId,
+                                      smsCode: smsCode);
+
+                              // Sign the user in (or link) with the credential
+                              final user =
+                                  await auth.signInWithCredential(credential);
+                              log('User: $user');
+                            }
+                          },
+                          codeAutoRetrievalTimeout: (String verificationId) {
+                            log('Code auto retrieval timeout: $verificationId');
+                          },
+                        );
+                      } else {
+                        log('Phone number is null');
                       }
                     },
-                    codeAutoRetrievalTimeout: (String verificationId) {
-                      log('Code auto retrieval timeout: $verificationId');
+                    child: const Text('Sign in with Phone Number'),
+                  ),
+                ] else ...[
+                  ElevatedButton(
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
                     },
-                  );
+                    child: const Text('Sign Out'),
+                  ),
+                  Text('User: $user'),
+                  ElevatedButton(
+                    child: const Text('GET /driver'),
+                    onPressed: () async {
+                      setState(() {
+                        getDriverResponse = null;
+                      });
+                      final token = await user!.getIdToken();
+                      log('Token: $token');
+                      final resp = await http.get(
+                        Uri.parse(
+                            'https://app-rpm-drive-api-dev.azurewebsites.net/api/v2/driver'),
+                        headers: {'Authorization': 'Bearer $token'},
+                      );
+                      final details = '${resp.statusCode} ${resp.body}';
 
-                  // if (code != null) {
-                  //   UserCredential creds = await auth.
-                  //   log('User: ${creds.user}');
-                  // } else {
-                  //   log('Code is null');
-                  // }
-                } else {
-                  log('Phone number is null');
-                }
-              },
-              child: const Text('Sign in with Phone Number'),
+                      log('GET /driver response: $details');
+
+                      setState(() {
+                        getDriverResponse = details;
+                      });
+                    },
+                  ),
+                  const Text('GET /driver response:'),
+                  Text(getDriverResponse ?? ''),
+                ],
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
